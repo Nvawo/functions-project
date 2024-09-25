64ae98d4e5ca62f8ed27174453917ed905da206f
unknown <nyawoalamar5@gmail.com>
2024-09-25 08:57:20 +0300

    first function

---
+++ main.js
@@ -0,0 +1,10 @@
+function calc(){
+    let a =document.getElementById("a").value;
+    let b =document.getElementById("b").value;
+    let c =document.getElementById("c").value;
+
+    let s =a*b*c/2;
+    let area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    document.getElementById("answer").value=area;
+    
+}
